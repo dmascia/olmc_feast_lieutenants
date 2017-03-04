0800892c2ebbac86d86135cae6154991ac407613
DaveMascia <dave@davemascia.com>
2017-03-04 15:41:32 -0500

    login routes, user model

---
+++ app/controllers/index.js
@@ -0,0 +1,37 @@
+"use strict";
+
+const express = require('express'),
+      router = express.Router();
+
+module.exports = (app, passport, ensureLogin) => {
+
+  app.use('/', router);
+
+  router.get('/', (req, res) => {
+
+    return res.render('index', {
+      csrfToken: req.csrfToken(),
+    });
+  });
+
+  router.post('/login', passport.authenticate('local', {
+    successRedirect: '/dashboard',
+    failureRedirect: '/',
+    failureFlash: 'Invalid credentials'
+  }));
+
+  router.get('/dashboard', ensureLogin.ensureLoggedIn('/'),
+    (req, res) => {
+
+      return res.render('dashboard');
+    }
+  );
+
+  router.get('/logout',  ensureLogin.ensureLoggedIn('/'),
+      (req, res) => {
+
+      req.logout();
+      return res.redirect('/');
+    }
+  );
+};
---
+++ app/models/Users.js
@@ -0,0 +1,99 @@
+"use strict";
+
+const crypto = require('crypto');
+
+module.exports = function(sequelize, DataTypes) {
+
+  const Users = sequelize.define('Users', {
+    id: {
+      type: DataTypes.INTEGER(11),
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    username_canonical: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email_canonical: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    enabled: {
+      type: DataTypes.INTEGER(1),
+      allowNull: false
+    },
+    salt: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    last_login: {
+      type: DataTypes.DATE,
+      allowNull: true
+    },
+    locked: {
+      type: DataTypes.INTEGER(1),
+      allowNull: false
+    },
+    expired: {
+      type: DataTypes.INTEGER(1),
+      allowNull: false
+    },
+    expires_at: {
+      type: DataTypes.DATE,
+      allowNull: true
+    },
+    confirmation_token: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    password_requested_at: {
+      type: DataTypes.DATE,
+      allowNull: true
+    },
+    roles: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    credentials_expired: {
+      type: DataTypes.INTEGER(1),
+      allowNull: false
+    },
+    credentials_expire_at: {
+      type: DataTypes.DATE,
+      allowNull: true
+    }
+  },
+  {
+    tableName: 'Users',
+    timestamps: false,
+  });
+
+  Users.verifyPasword = (user, enteredPassword) => {
+
+    const hashedPassword = crypto.createHash('md5')
+      .update(`${user.salt}${enteredPassword}`)
+      .digest('base64');
+
+    if (user.password === hashedPassword) {
+
+      return { match: true, user };
+    }
+
+    return { match: false };
+  };
+
+  return Users;
+};
